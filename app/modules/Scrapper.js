a23f586801b703f5fd9eb25ddc31b633fcab50ea
Loïc <loic.laurent@outlook.com>
2023-03-19 07:42:12 +0100

    liste des liens d'une page

--- app/modules/Scrapper.js
+++ app/modules/Scrapper.js
@@ -17,17 +17,38 @@ export class Scrapper {
         await this.browser.close();
     }
 
-    async scrap( url, parentLock ) {
+    async scrap( url, domainLock = true ) {
 
+        // domaine
+        const urlMatch = url.match(/^(?:https?:\/\/)?((?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+))/i);
+        const scheme = urlMatch[1];
+        const domain = urlMatch[2];
+        
         // chargement de la page
         const page = await this.browser.newPage();
         await page.goto( url );
         await page.waitForSelector('body'); // indicateur de chargement
 
-        const selector = !parentLock ? 'a' : `a[href^="${parentLock}"], a[href^="/"]`;
-        const allHrefs = await page.evaluate(() =>
+        process.stdout.write('.');
+
+        // liste de tous les liens
+        let allHrefs = await page.evaluate(() =>
             [...document.querySelectorAll( 'a' )].map(link => link.href)
         );
+
+        // réslution des URL relatives
+        allHrefs = allHrefs.map( a => {
+            const u = new URL( a, url );
+            return u.href;
+        } );
+
+        if( domainLock && !!domain ) {
+            allHrefs = allHrefs.filter( (a) => {
+                const uM = a.match(/^(?:https?:\/\/)?((?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+))/i);
+                const d = uM[2];
+                return domain === d;
+            })
+        }
 
         await page.close();
         return allHrefs;
